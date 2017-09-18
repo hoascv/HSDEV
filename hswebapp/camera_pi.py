3daa2e6819ecbec4a1b1ed328ea4f253817e8436
HELDER <hoascv@gmx.com>
2017-09-18 15:28:32 +0200

    add datetime to streaming

--- hswebapp/camera_pi.py
+++ hswebapp/camera_pi.py
@@ -1,8 +1,9 @@
 import io
 import time
 import picamera
+from picamera import Color
 from base_camera import BaseCamera
-
+import datetime as dt
 
 class Camera(BaseCamera):
     @staticmethod
@@ -12,8 +13,12 @@ class Camera(BaseCamera):
             time.sleep(2)
 
             stream = io.BytesIO()
+            camera.annotate_foreground = Color('black')
+            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            
             for foo in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
+                camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
